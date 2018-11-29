05244b889c98e03ab17e8fa5d8184b5d85259e7e
Louie Miranda <lmiranda@gmail.com>
2018-11-29 23:55:44 +0800

    Addition of receiving

--- api.go
+++ api.go
@@ -20,6 +20,7 @@ func main() {
 	r.HandleFunc("/", HomeHandler)
 
 	r.HandleFunc("/api/create/sqs", CreateSQSHandler)
+	r.HandleFunc("/api/receive/sqs", ReceiveSQSHandler)
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
@@ -31,6 +32,8 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateSQSHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("RECEIVING...")
+
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
@@ -71,4 +74,56 @@ func CreateSQSHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	// io.WriteString(w, `{"result": true}`)
 	io.WriteString(w, *result.MessageId)
+}
+
+func ReceiveSQSHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PROCESSING...")
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := sqs.New(sess)
+
+	qURL := "https://sqs.ap-southeast-1.amazonaws.com/799216407651/test"
+
+	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            &qURL,
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   aws.Int64(20), // 20 seconds
+		WaitTimeSeconds:     aws.Int64(0),
+	})
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	if len(result.Messages) == 0 {
+		fmt.Println("Received no messages")
+		return
+	}
+
+	resultDelete, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
+		QueueUrl:      &qURL,
+		ReceiptHandle: result.Messages[0].ReceiptHandle,
+	})
+
+	if err != nil {
+		fmt.Println("Delete Error", err)
+		return
+	}
+
+	fmt.Println("Message Deleted", resultDelete)
+
+	// w.WriteHeader(http.StatusOK)
+	// w.Header().Set("Content-Type", "application/json")
+	// // io.WriteString(w, `{"result": true}`)
+	// io.WriteString(w, resultDelete)
 }
