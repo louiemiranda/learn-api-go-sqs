36cf09701153af1013b9fecee71c3dba7f920b25
Louie Miranda <lmiranda@gmail.com>
2018-11-30 15:53:44 +0800

    Added TODO for mysql. Get status. Run worker, even if not main package?

--- api.go
+++ api.go
@@ -16,11 +16,12 @@ func main() {
 	fmt.Println("STARTING API LOADER...")
 
 	r := mux.NewRouter()
-	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", HomeHandler)
 
-	r.HandleFunc("/api/create/sqs", CreateSQSHandler)
-	r.HandleFunc("/api/receive/sqs", ReceiveSQSHandler)
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/api/sqs", CreateSQSHandler).
+		Methods("POST")
+	r.HandleFunc("/api/sqs/{id}", StatusHandler).
+		Methods("GET")
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
@@ -68,6 +69,8 @@ func CreateSQSHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// @TODO -- insert to mysql db and return a reference
+
 	fmt.Println("Success", *result.MessageId)
 
 	w.WriteHeader(http.StatusOK)
@@ -76,51 +79,10 @@ func CreateSQSHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, *result.MessageId)
 }
 
-func ReceiveSQSHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("PROCESSING...")
+func StatusHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("VERIFYING STATUS...")
 
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-
-	svc := sqs.New(sess)
-
-	qURL := "https://sqs.ap-southeast-1.amazonaws.com/799216407651/test"
-
-	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
-		AttributeNames: []*string{
-			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
-		},
-		MessageAttributeNames: []*string{
-			aws.String(sqs.QueueAttributeNameAll),
-		},
-		QueueUrl:            &qURL,
-		MaxNumberOfMessages: aws.Int64(1),
-		VisibilityTimeout:   aws.Int64(20), // 20 seconds
-		WaitTimeSeconds:     aws.Int64(0),
-	})
-
-	if err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-
-	if len(result.Messages) == 0 {
-		fmt.Println("Received no messages")
-		return
-	}
-
-	resultDelete, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
-		QueueUrl:      &qURL,
-		ReceiptHandle: result.Messages[0].ReceiptHandle,
-	})
-
-	if err != nil {
-		fmt.Println("Delete Error", err)
-		return
-	}
-
-	fmt.Println("Message Deleted", resultDelete)
+	// @TODO -- connect to mysql db and return status
 
 	// w.WriteHeader(http.StatusOK)
 	// w.Header().Set("Content-Type", "application/json")
---
+++ worker/worker.go
@@ -0,0 +1,63 @@
+package worker
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func main() {
+	fmt.Println("WORKER PROCESSOR...")
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := sqs.New(sess)
+
+	qURL := "https://sqs.ap-southeast-1.amazonaws.com/799216407651/test"
+
+	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            &qURL,
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   aws.Int64(20), // 20 seconds
+		WaitTimeSeconds:     aws.Int64(0),
+	})
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	if len(result.Messages) == 0 {
+		fmt.Println("Received no messages")
+		return
+	}
+
+	resultDelete, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
+		QueueUrl:      &qURL,
+		ReceiptHandle: result.Messages[0].ReceiptHandle,
+	})
+
+	if err != nil {
+		fmt.Println("Delete Error", err)
+		return
+	}
+
+	// @TODO -- mysql, update status of request
+
+	fmt.Println("Message Deleted", resultDelete)
+
+	// w.WriteHeader(http.StatusOK)
+	// w.Header().Set("Content-Type", "application/json")
+	// // io.WriteString(w, `{"result": true}`)
+	// io.WriteString(w, resultDelete)
+}
